067df1752b0078f9430f54a59b54ec28c3ff3bf7
kevin <kipronok234@gmail.com>
2024-07-13 17:37:10 +0300

    phase-1-practice-dog-ceo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+function fetchImages () {
+    const apiURL = "https://dog.ceo/api/breeds/image/random/4";
+
+    fetch(apiURL)
+       .then(response => response.json())
+       .then(data => {
+         const iamageCointainer = document.getElementById("image-container");
+         data.message.forEach(imageUrl =>  {
+            const imageElement = document.createElement("img");
+            imageElement.src = imageUrl;
+            imageContainer.appendChild(imgElement);
+         })          
+         });
+         .catch(error =>  console.error("Error fetching images :",error));
+       })
+}
